3a2bfd5695d9f8ddd8942db9cbeea07b290121ad
unknown <thomas de bonnet>
2015-05-16 10:43:31 +0200

    major push

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -5,7 +5,7 @@ if not settings.DEBUG:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(settings.BASE_DIR, 'db.sqlite3'),
         }
     }
 
