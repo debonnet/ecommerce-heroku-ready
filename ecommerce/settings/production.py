a806098d7c59b67d49ffeb120edad7ff50dd6850
unknown <thomas de bonnet>
2015-05-16 18:49:09 +0200

    production server

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -22,7 +22,7 @@ if not settings.DEBUG:
     DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(settings.BASE_DIR, 'db2.sqlite3'),
+        'NAME': os.path.join(settings.BASE_DIR, 'db.sqlite3'),
         }
     }
 
